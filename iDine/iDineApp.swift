fd5b491548c26aa857829fa209eb9d911fa71be4
KiranJungGurung <kirangurung394@gmail.com>
2022-12-30 13:13:43 +0545

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Kiran Gurung on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
